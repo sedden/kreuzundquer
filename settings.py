d681363703f936be6a819eb6331d3bbe4a49f702
Stefan Jenkner <stefan@jenkner.org>
2010-02-27 11:21:23 +0100

    'dbtemplates' eingabaut und Templates optimiert

--- settings.py
+++ settings.py
@@ -83,7 +83,7 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.load_template_source',
     'django.template.loaders.app_directories.load_template_source',
     'django.template.loaders.eggs.load_template_source',
-#    'dbtemplates.loader.load_template_source',
+    'dbtemplates.loader.load_template_source',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
@@ -137,4 +137,5 @@ INSTALLED_APPS = (
     'tagging',
     'gravatar',
     'robots',
+    'dbtemplates',
 )
