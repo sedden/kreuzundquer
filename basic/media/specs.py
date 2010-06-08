8311043314d84d8df689f767c92c1b35e69e886c
Stefan Jenkner <stefan@jenkner.org>
2010-06-08 21:14:46 +0200

    Templates und Integration von 'basic.media'

--- basic/media/specs.py
+++ basic/media/specs.py
@@ -1,20 +1,40 @@
-from imagekit.specs import ImageSpec 
-from imagekit import processors 
+from imagekit.specs import ImageSpec
+from imagekit import processors
 
-class ResizeThumb(processors.Resize): 
-    width = 460
-    height = 306
+class ResizeThumb(processors.Resize):
+    width = 136
+    height = 102
+    crop = True
+
+class ResizeRegular(processors.Resize):
+    width = 296
+    height = 296
     crop = False
 
 class ResizeDisplay(processors.Resize):
-    width = 1024
+    width = 800
+    height = 600
+    crop = False
 
-class EnchanceThumb(processors.Adjustment): 
-    sharpness = 1.1 
+class EnchanceThumb(processors.Adjustment):
+    sharpness = 1.5
+    color = 0.0
 
-class Thumbnail(ImageSpec): 
-    pre_cache = True 
-    processors = [processors.Transpose, ResizeThumb, EnchanceThumb] 
+class EnchanceThumbHover(processors.Adjustment):
+    sharpness = 1.5
+    color = 1.5
+
+class Thumbnail(ImageSpec):
+    pre_cache = True
+    processors = [processors.Transpose, ResizeThumb, EnchanceThumb]
+
+class Thumbnail_Hover(ImageSpec):
+    pre_cache = True
+    processors = [processors.Transpose, ResizeThumb, EnchanceThumbHover]
+
+class Regular(ImageSpec):
+    pre_cache = True
+    processors = [processors.Transpose, ResizeRegular]
 
 class Display(ImageSpec):
     processors = [processors.Transpose, ResizeDisplay]
--- settings.py
+++ settings.py
@@ -138,4 +138,7 @@ INSTALLED_APPS = (
     'gravatar',
     'robots',
     'dbtemplates',
+    'imagekit',
+    'basic.media',
+    'basic.inlines',
 )
